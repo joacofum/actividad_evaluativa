34435436f37a3696ac6ecacc98fcaac6dc891525
joacofum <joacofum@gmail.com>
2022-06-29 11:36:12 -0300

    ejercicio 2

--- src/main/java/ejercicio1/main.java
+++ src/main/java/ejercicio1/main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
+        double km;
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese un número");
-        double numero = entrada.nextDouble();
-        if(numero % 2 == 0){
-            System.out.println("El número ingresado es par");
-        }else{
-            System.out.println("El número ingresado es impar");
-        }
+        do{
+            System.out.println("Ingrese kilómetros, si ingresa 0 saldrá del programa: ");
+            km = entrada.nextDouble();
+            System.out.println("La conversión de kilómetros a millas es: " + km * 1.6093);
+        }while(km != 0);
+
     }
 }
---
+++ src/main/java/ejercicio2/main.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        double numero = entrada.nextDouble();
+        if(numero % 2 == 0){
+            System.out.println("El número ingresado es par");
+        }else{
+            System.out.println("El número ingresado es impar");
+        }
+    }
+}
